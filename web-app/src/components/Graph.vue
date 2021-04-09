71e0b9ce14f9ee0c4f047dc6a52eadd5c42abbab
Trevor Corcoran <supershadowplay@gmail.com>
2021-04-09 14:42:26 -0400

    Graph.vue added

---
+++ web-app/src/components/Graph.vue
@@ -0,0 +1,39 @@
+<template>
+    <VueApexCharts width="500" type="bar" :options="chartOptions" :series="series"/>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+export default {
+  components: {
+    VueApexCharts
+  },
+  props: {
+    name: {
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      chartOptions: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: ['June', 'July', 'August']
+        }
+      },
+      series: [
+        {
+          name: 'Average High',
+          data: [80, 91, 86]
+        },
+        {
+          name: 'Average Low',
+          data: [56, 70, 66]
+        }
+      ]
+    }
+  }
+}
+</script>
