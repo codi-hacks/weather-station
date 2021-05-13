89561e50b33fd8f75a47995c78497ff5aac1547e
Christopher Carrillo <christopher.a.carrillo@gmail.com>
2021-05-13 14:01:28 -0400

    Created stations routes

---
+++ web-app/src/views/Station.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ params }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Station',
+  data() {
+    return {
+    }
+  },
+  computed: {
+    params() {
+      return this.$route.params
+    }
+  }
+}
+</script>
