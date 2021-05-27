a6d40f3049dde7a330d98876105841458e946830
Jay Thomas <jay@gfax.ch>
2021-05-27 13:54:41 -0400

    Create API_URL environment variable

--- web-app/.eslintrc.js
+++ web-app/.eslintrc.js
@@ -7,12 +7,18 @@ module.exports = {
     'plugin:vue/essential',
     '@vue/standard'
   ],
+  globals: {
+    API_URL: true
+  },
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
+    'comma-dangle': 'warn',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': ['error', 'never']
+    'no-unused-vars': 'warn',
+    'space-before-function-paren': ['error', 'never'],
+    'spaced-comment': 'off'
   }
 }
