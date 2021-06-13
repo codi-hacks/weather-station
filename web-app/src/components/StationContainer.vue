66b466f9728f7f5f941abcfbff7fe7bc45eeca3d
marcioRene <69165341+marcioRene@users.noreply.github.com>
2021-06-13 15:26:04 -0600

    Update graph and station page (#25)

--- web-app/src/components/StationContainer.vue
+++ web-app/src/components/StationContainer.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    Successfully loaded {{ station.label }}!!
+  <div  class="flex-container">
+        <v-card
+      class="flex-item"
+      rounded
+      v-for="sensor in stationSensors"
+      :key="sensor.id">
+      <Graph :name = "sensor.label" :measurements="sensor.measurements" :sensor-type="sensor.type" />
+        </v-card>
   </div>
 </template>
 
 <script>
-
+import Graph from './Graph'
 export default {
+  components: {
+    Graph
+  },
   props: {
     station: {
       required: true,
@@ -28,4 +37,51 @@ export default {
 </script>
 
 <style scoped>
+.flex-container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+}
+.flex-item {
+  flex-basis: 25%;
+  flex-grow: 0.35;
+  flex-shrink: 0;
+  margin-bottom: 2px;
+  margin-top: 2px;
+  min-height: 20em;
+  width: 25%;
+}
+@media screen and (max-width: 1800px) {
+  .flex-item {
+    flex-basis: 33.33%;
+    width: 33.33%;
+  }
+}
+@media screen and (max-width: 860px) {
+  .flex-item {
+    flex-basis: 50%;
+    width: 50%;
+  }
+}
+@media screen and (max-width: 640px) {
+  .flex-item {
+    flex-basis: 100%;
+    width: 100%;
+  }
+}
+/deep/ .bookmark-button {
+  bottom: 0;
+  display: none;
+  padding-bottom: 4px;
+  padding-right: 4px;
+  position: absolute;
+  right: 0;
+  z-index: 1;
+}
+/deep/ .card-container {
+  height: 100%;
+}
+/deep/ .card-container:hover .bookmark-button {
+  display: block;
+}
 </style>
