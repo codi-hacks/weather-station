0e26fb55d0e3adf2be29680b2fa786fee9304f9b
Jay Thomas <jay@gfax.ch>
2021-06-30 17:21:53 -0400

    Fix error handler message

--- api-server/src/error_handler.rs
+++ api-server/src/error_handler.rs
@@ -31,7 +31,7 @@ impl From<DieselError> for CustomError {
         match error {
             DieselError::DatabaseError(_, err) => CustomError::new(409, err.message().to_string()),
             DieselError::NotFound => {
-                CustomError::new(404, "The employee record not found".to_string())
+                CustomError::new(404, "Record not found".to_string())
             }
             err => CustomError::new(500, format!("Unknown Diesel error: {}", err)),
         }
