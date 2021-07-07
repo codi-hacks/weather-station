2b61f2013b25c465db80fdabee99eab42094ee84
Christopher Carrillo <christopher.a.carrillo@gmail.com>
2021-07-07 16:09:17 -0400

    Removed all routes except get from sensors and stations routes.rs files. Use for testing before pushing to api.

--- api-server/src/stations/routes.rs
+++ api-server/src/stations/routes.rs
@@ -1,6 +1,6 @@
 use crate::stations::{StationsChangeset, StationsModel};
 use crate::error_handler::CustomError;
-use actix_web::{delete, get, post, put, web, HttpResponse};
+use actix_web::{get, web, HttpResponse};
 use serde_json::json;
 
 #[get("/stations")]
@@ -15,31 +15,7 @@ async fn find(id: web::Path<uuid::Uuid>) -> Result<HttpResponse, CustomError> {
     Ok(HttpResponse::Ok().json(stations))
 }
 
-#[post("/stations")]
-async fn create(stations : web::Json<StationsChangeset>) -> Result<HttpResponse, CustomError> {
-    let station = StationsModel::create(stations.into_inner())?;
-    Ok(HttpResponse::Ok().json(station))
-}
-
-#[put("/stations/{id}")]
-async fn update(
-    id: web::Path<uuid::Uuid>,
-    station: web::Json<StationsChangeset>,
-) -> Result<HttpResponse, CustomError> {
-    let station = StationsModel::update(id.into_inner(), station.into_inner())?;
-    Ok(HttpResponse::Ok().json(station))
-}
-
-#[delete("/stations/{id}")]
-async fn delete(id: web::Path<uuid::Uuid>) -> Result<HttpResponse, CustomError> {
-    let deleted_station = StationsModel::delete(id.into_inner())?;
-    Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_station })))
-}
-
 pub fn init_routes(comfig: &mut web::ServiceConfig) {
     comfig.service(find_all);
     comfig.service(find);
-    comfig.service(create);
-    comfig.service(update);
-    comfig.service(delete);
 }
