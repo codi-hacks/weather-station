797f17a0678c939ab8f12802ce9f6316bbc4607c
Christopher Carrillo <46425604+Chrizafier@users.noreply.github.com>
2021-07-07 15:57:35 -0400

    Added nested data into GET /sensors/{id} (#29)
    * Nested data in GET /sensors/{id}
    * Added measurements to the sensor endpoint
    * Fixed small typos and whitespace issues

--- api-server/src/sensors/model.rs
+++ api-server/src/sensors/model.rs
@@ -3,11 +3,24 @@ use crate::error_handler::CustomError;
 use crate::schema::sensors;
 use crate::sensor_types::{SensorTypesModel};
 use crate::stations::{StationsModel};
+use crate::measurements::MeasurementsModel;
 
 use chrono::{Local, NaiveDateTime};
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+
+#[derive(Deserialize, Serialize)] 
+pub struct Sensor {
+    pub id: Uuid,
+    pub alias: String,
+    pub label: String,
+    pub type_id: Uuid,
+    pub station_id: Uuid,
+    pub created_at: chrono::NaiveDateTime,
+    pub updated_at: chrono::NaiveDateTime,
+    pub measurements: Vec<MeasurementsModel>
+}
 
 #[derive(Serialize, Deserialize, AsChangeset, Insertable)]
 #[table_name = "sensors"]
@@ -18,12 +31,13 @@ pub struct SensorsChangeset {
     pub station_id: Uuid
 }
 
-#[derive(Serialize, Deserialize, Associations, Queryable, Insertable, Identifiable)]
+
+#[derive(AsChangeset, Associations, Clone, Deserialize, Identifiable, Insertable, Queryable, Serialize)] 
 #[belongs_to(SensorTypesModel, foreign_key = "type_id")]
 #[belongs_to(StationsModel, foreign_key = "station_id")]
 #[table_name = "sensors"]
 pub struct SensorsModel {
-    pub id: uuid::Uuid,
+    pub id: Uuid,
     pub alias: String,
     pub label: String,
     pub type_id: Uuid,
@@ -39,10 +53,20 @@ impl SensorsModel {
         Ok(sensors)
     }
 
-    pub fn find(id: uuid::Uuid) -> Result<Self, CustomError> {
+    pub fn find(id: Uuid) -> Result<Sensor, CustomError> {
         let conn = db::connection()?;
-        let sensor = sensors::table.filter(sensors::id.eq(id)).first(&conn)?;
-        Ok(sensor)
+        let sensor: Self = sensors::table.filter(sensors::id.eq(id)).first(&conn)?;
+        let measurements: Vec<MeasurementsModel> = MeasurementsModel::belonging_to(&sensor).load(&conn)?;
+        Ok(Sensor {
+            id:             sensor.id,
+            alias:          sensor.alias,
+            label:          sensor.label,
+            type_id:        sensor.type_id,
+            station_id:     sensor.station_id,
+            created_at:     sensor.created_at,
+            updated_at:     sensor.updated_at,
+            measurements
+        })
     }
 
     pub fn create(sensor: SensorsChangeset) -> Result<Self, CustomError> {
@@ -66,7 +90,7 @@ impl SensorsModel {
         Ok(sensor)
     }
 
-    pub fn update(id: uuid::Uuid, sensor: SensorsChangeset) -> Result<Self, CustomError> {
+    pub fn update(id: Uuid, sensor: SensorsChangeset) -> Result<Self, CustomError> {
         use crate::schema::sensors::dsl::{
             alias as alias_column,
             label as label_column,
@@ -100,7 +124,7 @@ impl SensorsModel {
         Ok(sensor)
     }
 
-    pub fn delete(id: uuid::Uuid) -> Result<usize, CustomError> {
+    pub fn delete(id: Uuid) -> Result<usize, CustomError> {
         let conn = db::connection()?;
         let res = diesel::delete(sensors::table.filter(sensors::id.eq(id))).execute(&conn)?;
         Ok(res)
