215c2320cd0f9d46364fe5033d04834fcf37239a
Jay Thomas <jay@gfax.ch>
2021-07-09 11:46:37 -0400

    Remove dead code

--- api-server/src/measurements/model.rs
+++ api-server/src/measurements/model.rs
@@ -1,36 +1,27 @@
-use chrono::NaiveDateTime;
 use crate::db;
 use crate::error_handler::CustomError;
 use crate::schema::measurements;
+use crate::sensors::{SensorsModel};
+use bigdecimal::BigDecimal;
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
-use bigdecimal::BigDecimal;
-use crate::sensors::{SensorsModel};
 use uuid::Uuid;
 
-#[derive(AsChangeset, Associations, Clone, Deserialize, Identifiable, Insertable, Queryable, Serialize)] 
+#[derive(AsChangeset, Associations, Deserialize, Identifiable, Insertable, Queryable, Serialize)]
 #[belongs_to(SensorsModel, foreign_key = "sensor_id")]
 #[table_name = "measurements"]
 pub struct MeasurementsModel {
     pub id: Uuid,
     pub value: BigDecimal,
+    // Measurements only come back inside sensor data so this
+    // information is redundant from a JSON perspective.
+    #[serde(skip_serializing)]
     pub sensor_id: Uuid,
     pub created_at: NaiveDateTime
 }
 
 impl MeasurementsModel {
-    pub fn find_all() -> Result<Vec<Self>, CustomError> {
-        let conn = db::connection()?;
-        let measurements = measurements::table.load::<Self>(&conn)?;
-        Ok(measurements)
-    }
-
-    pub fn find(id: Uuid) -> Result<Self, CustomError> {
-        let conn = db::connection()?;
-        let measurement = measurements::table.filter(measurements::id.eq(id)).first(&conn)?;
-        Ok(measurement)
-    }
-
     pub fn create(sensor_id: Uuid, value: BigDecimal) -> Result<Self, CustomError> {
         let conn = db::connection()?;
         let measurement = diesel::insert_into(measurements::table)
@@ -41,11 +32,5 @@ impl MeasurementsModel {
             .get_result(&conn)?;
         SensorsModel::touch(sensor_id)?;
         Ok(measurement)
-    }
-
-    pub fn delete(id: Uuid) -> Result<usize, CustomError> {
-        let conn = db::connection()?;
-        let res = diesel::delete(measurements::table.filter(measurements::id.eq(id))).execute(&conn)?;
-        Ok(res)
     }
 }
--- api-server/src/sensor_types/model.rs
+++ api-server/src/sensor_types/model.rs
@@ -1,67 +1,53 @@
 use crate::db;
 use crate::error_handler::CustomError;
 use crate::schema::sensor_types;
+use crate::sensors::SensorsModel;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
+use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, AsChangeset, Insertable)]
-#[table_name = "sensor_types"]
-pub struct SensorTypesChangeset {
+#[skip_serializing_none]
+#[derive(Deserialize, Serialize, Identifiable)]
+pub struct SensorType {
+    pub id: Uuid,
     pub label: String,
-    pub description: String
+    pub description: String,
+    pub sensors: Option<Vec<SensorsModel>>
 }
 
-#[derive(Serialize, Deserialize, Queryable, Insertable)]
+#[derive(AsChangeset, Associations, Clone, Deserialize, Identifiable, Insertable, Queryable, Serialize)]
 #[table_name = "sensor_types"]
 pub struct SensorTypesModel {
-    pub id: uuid::Uuid,
+    pub id: Uuid,
     pub label: String,
     pub description: String
 }
 
 impl SensorTypesModel {
-    pub fn find_all() -> Result<Vec<Self>, CustomError> {
+    pub fn find_all() -> Result<Vec<SensorType>, CustomError> {
         let conn = db::connection()?;
-        let sensor_types = sensor_types::table.load::<Self>(&conn)?;
+        let sensor_types = sensor_types::table.load::<SensorTypesModel>(&conn)?;
+        let sensor_types = sensor_types.into_iter().map(|sensor_type| {
+            SensorType {
+                id: sensor_type.id,
+                label: sensor_type.label,
+                description: sensor_type.description,
+                sensors: None
+            }
+        }).collect();
         Ok(sensor_types)
     }
 
-    pub fn find(id: uuid::Uuid) -> Result<Self, CustomError> {
+    pub fn find(id: Uuid) -> Result<SensorType, CustomError> {
         let conn = db::connection()?;
-        let sensor_type = sensor_types::table.filter(sensor_types::id.eq(id)).first(&conn)?;
-        Ok(sensor_type)
-    }
-
-    pub fn create(sensor_type: SensorTypesChangeset) -> Result<Self, CustomError> {
-        let conn = db::connection()?;
-        let sensor_type = SensorTypesChangeset::from(sensor_type);
-        let sensor_type = diesel::insert_into(sensor_types::table)
-            .values(sensor_type)
-            .get_result(&conn)?;
-        Ok(sensor_type)
-    }
-
-    pub fn update(id: uuid::Uuid, sensor_type: SensorTypesChangeset) -> Result<Self, CustomError> {
-        let conn = db::connection()?;
-        let sensor_type = diesel::update(sensor_types::table)
-            .filter(sensor_types::id.eq(id))
-            .set(sensor_type)
-            .get_result(&conn)?;
-        Ok(sensor_type)
-    }
-
-    pub fn delete(id: uuid::Uuid) -> Result<usize, CustomError> {
-        let conn = db::connection()?;
-        let res = diesel::delete(sensor_types::table.filter(sensor_types::id.eq(id))).execute(&conn)?;
-        Ok(res)
-    }
-}
-
-impl SensorTypesChangeset {
-    fn from(sensor_type: SensorTypesChangeset) -> SensorTypesChangeset {
-        SensorTypesChangeset {
+        let sensor_type: SensorTypesModel = sensor_types::table.filter(sensor_types::id.eq(id)).first(&conn)?;
+        let sensors: Vec<SensorsModel> = SensorsModel::belonging_to(&sensor_type).load(&conn)?;
+        Ok(SensorType {
+            id: sensor_type.id,
             label: sensor_type.label,
-            description: sensor_type.description
-        }
+            description: sensor_type.description,
+            sensors: Some(sensors)
+        })
     }
 }
